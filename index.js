856eeba7a4602c4a24b57bcc34a5a622abc12114
mahid <mo47da@gmail.com>
2019-12-19 00:56:19 +0600

    tried to add serverless

--- index.js
+++ index.js
@@ -1,85 +1,73 @@
 //jshint esversion:8
 const RedditScraper = require("reddit-scraper");
-const url = require('url');
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config();  //this is fro hiding secret in .env file
+const NodeCache = require("node-cache");
+const cache = new NodeCache({
+  stdTTL: 0
+});
 
 
+const redditScraperOptions = {
+  AppId: "YZwduUlkpuIkJw",
+  AppSecret: "ZcS65djwnC5nT_GmlH4PbPD8SsI"
+};
+const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
+console.log("Configuration Loaded!");
 
-app.set('view engine', 'ejs');
-(async () => {
-  const redditScraperOptions = {
+const scrapedata = async SubReddit => {
+  try {
+    const pages = 4;
 
-    AppId: process.env.API_ID ,                           // enter the id here
-    AppSecret: process.env.API_KEY                     // enter the secret here
-
-  };
-
-  const pgNum = 6 ;
-  const sortType = "top" ;
-  const recordNum = 25;
-
-    class Memeobj
-   {
-    constructor(Pages,Records,SubReddit,SortType,)
-     {
-      this.Pages = Pages ;
-      this.Records = Records;
-      this.SortType = SortType;
-      this.SubReddit = SubReddit;
-     }
-   }
-    const memes          = await new Memeobj(pgNum,recordNum,"memes",sortType);
-    const dank_Meme      = await new Memeobj(pgNum,recordNum,"dank_meme",sortType);
-    const deepFriedMemes = await new Memeobj(pgNum,recordNum,"deepfriedmemes",sortType);
-    const memeEconomy    = await new Memeobj(pgNum,recordNum,"MemeEconomy",sortType);
-
-  try
-  {
-    const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
-    console.log("Configuration Loaded!");
-
-  /*  var memeData = await redditScraper.scrapeData(meme);
-    console.log("Meme Subreddit Scraped!");
-  */
-    var memesData = await redditScraper.scrapeData(memes);
-    console.log("Memes Subreddit Scraped!");
-    var dank_MemesData = await redditScraper.scrapeData(dank_Meme);
-    console.log("Dank_Memes Subreddit Scraped!");
-    var deepFriedMemesData = await redditScraper.scrapeData(deepFriedMemes);
-    console.log("DeepFriedMemes Subreddit Scraped!");
-    var memeEconomyData = await redditScraper.scrapeData(memeEconomy);
-    console.log("MemeEconomy Subreddit Scraped!");
-
-    const scrapedData = [].concat.apply([], [/*memeData,*/ memesData,dank_MemesData,deepFriedMemesData,memeEconomyData ]);
-    var memeCount = 0;
-    var skipCount = 0;
-    var invalidCount = 0;
-    var url = [] ;
-    for ( var i = 0; i < scrapedData.length; i++)
-    {
-       url.push(scrapedData[i].data.url);
-      console.log(url);
+    class Memeobj {
+      constructor(subreddit) {
+        this.Pages = 1;
+        this.Records = 25;
+        this.SortType = "top";
+        this.SubReddit = subreddit;
+        this.Info = null;
+        this.urls = [];
+      }
     }
-    console.log(scrapedData.length + " total memes fetched.");
-  } catch (error) {
-    console.error(error);
+
+    const obj = new Memeobj(SubReddit);
+    obj.Info = await redditScraper.scrapeData(obj);
+    obj.urls = await obj.Info.map(obj => obj.data.url);
+    var imgUrl = obj.urls.filter(name => name.includes('.jpg') || name.includes('.png') || name.includes('.jpeg') || name.includes('.gif'));
+    return imgUrl;
+  } catch (err) {
+    console.log(err);
   }
-  app.get("/", function(req, res) {
-    res.render("index", {
-      url: url
-    });
-  });
+};
 
-  app.get("/apis", (req, res) => {
-  res.json(
-    url
-  );
-  });
+const set = async (req, res, next) => {
+  try {
+    const sub = req.params.sub;
+    const Urls = await scrapedata(sub);
+    const urls = JSON.stringify(Urls);
+    console.log(Urls);
+    console.log(typeof (urls));
+    cache.set(sub, urls);
+    return res.send(Urls);
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+};
 
-var port = process.env.PORT || 3000;
-  app.listen(port, function() {
-    console.log('server has started in ' + port);
-  });
-})();
+const get = (req, res, next) => {
+  const sub = req.params.sub;
+  const content = cache.get(sub);
+  if (content) {
+    return res.status(200).send(JSON.parse(content));
+  }
+  return next();
+};
+
+app.get("/:sub", get, set);
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`server has started in ${port}`);
+});

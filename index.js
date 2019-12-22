e59ac2f0e7692942a9462eb59334b023e4a12109
mahid <mo47da@gmail.com>
2019-12-22 18:39:25 +0600

    added

--- index.js
+++ index.js
@@ -4,48 +4,67 @@ const express = require("express");
 const app = express();
 const NodeCache = require("node-cache");
 const cache = new NodeCache({
-  stdTTL: 0
+  stdTTL: 40000
 });
-
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'public,max-age=31600');
+  next()
+})
 
 const redditScraperOptions = {
-  AppId: "YZwduUlkpuIkJw",
-  AppSecret: "ZcS65djwnC5nT_GmlH4PbPD8SsI"
+  AppId: "atU4rU0qYTpyjA",
+  AppSecret: "G6mDHqkYPR4CUiwSjyJJzVJXv6s"
 };
 const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
 console.log("Configuration Loaded!");
 
+
+
+app.use(express.json());
+
+
 const scrapedata = async (SubReddit) => {
   try {
-
     class Memeobj {
-      constructor(subreddit) {
-        this.Pages = 4;
+      constructor(sub) {
+        this.Pages = 1;
         this.Records = 25;
         this.SortType = "top";
-        this.SubReddit = subreddit;
-        this.Info = null;
-        this.urls = [];
+        this.SubReddit = sub;
       }
     }
-
     const obj = new Memeobj(SubReddit);
-    obj.Info = await redditScraper.scrapeData(obj);
-    obj.urls = await obj.Info.map(obj => obj.data.url);
-    const imgUrl = obj.urls.filter(name => name.includes('.jpg') || name.includes('.png') || name.includes('.jpeg') || name.includes('.gif'));
+    const postData = await redditScraper.scrapeData(obj);
+    const postUrls = await postData.map(obj => obj.data.url);
+    const imgUrl = await postUrls.filter(name => name.includes('.jpg') || name.includes('.png') || name.includes('.jpeg') || name.includes('.gif'));
     return imgUrl;
   } catch (err) {
     console.log(err);
   }
 };
 
-const set = async (req, res, next) => {
-  try {
 
-    const Urls = await scrapedata(req.params.sub);
+const get = (req, res, next) => {
+  const sub = req.params.sub;
+  if (cache.has(sub)) {
+    const content = JSON.parse(cache.get(sub));
+    return res.status(200).send(content);
+  } else {
+    return next();
+  };
+};
+
+const set = async (req, res, next) => {
+  const sub = req.params.sub;
+  if (cache.has(sub)) {
+    const content = JSON.parse(cache.get(sub));
+    return res.status(200).send(content);
+  };
+
+  try {
+    const Urls = await scrapedata(sub);
     const urls = JSON.stringify(Urls);
     console.log(Urls);
-    console.log(typeof (urls));
     cache.set(sub, urls);
     return res.send(Urls);
     next();
@@ -55,15 +74,12 @@ const set = async (req, res, next) => {
   }
 };
 
-const get = (req, res, next) => {
-  const content = cache.get(req.params.sub);
-  if (content) {
-    return res.status(200).send(JSON.parse(content));
-  }
-  return next();
-};
-
 app.get("/:sub", get, set);
+
+app.get("/", (req, res) => {
+  res.send('type subredit after /');
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {

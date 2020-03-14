3e396c675efb73a3d606e1a78f5e7b512e8b6fb6
mahid <mo47da@gmail.com>
2020-03-14 11:00:28 +0600

    starting again

--- scrapedata.js
+++ scrapedata.js
@@ -1,25 +1,23 @@
-const { redditScraper } = require("./index");
-const scrapedata = async (SubReddit, page = 4) => {
-  try {
-    class Memeobj {
-      constructor(Sub) {
-        this.Pages = page;
-        this.Records = 25;
-        this.SortType = "top";
-        this.SubReddit = Sub;
-      }
+const {redditScraper} = require("./index");
+const scrapedata = async (SubReddit,page=4) => {
+    try {
+        class Memeobj {
+            constructor(Sub) {
+                this.Pages = page;
+                this.Records = 25;
+                this.SortType = "top";
+                this.SubReddit = Sub;
+                /* this.Info = null; */
+                /* this.urls = []; */
+            }
+        };
+        const obj = new Memeobj(SubReddit);
+        const memeData = await redditScraper.scrapeData(obj);
+        const memeUrls = await memeData.map(obj => obj.data.url);
+        const imgUrl = memeUrls.filter(name => name.match(/\.(gif|jpeg|jpg|png)$/ig));
+        return imgUrl;
+    } catch (err) {
+        console.log('error in scrape');
     }
-    const obj = new Memeobj(SubReddit);
-    const memeData = await redditScraper.scrapeData(obj);
-    const memeDat = await memeData.filter((item) =>
-      item.data.post_hint.includes("image")
-    );
-    //  const imgUrl = memeUrls.filter((name) =>name.match(/\.(gif|jpeg|jpg|png)$/gi));
-    const memeUrl = await memeDat.map((obj) => obj.data.url);
-   // const memeTitle = await memeDat.map((obj) => obj.data.title);
-    return { memeUrl };
-  } catch (err) {
-    console.log(err);
-  }
 };
 exports.scrapedata = scrapedata;

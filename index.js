c784461b18c23f69c300bf3e11d5bafddde4866a
mahid <mo47da@gmail.com>
2020-03-15 12:19:07 +0600

    broke url and title into micro services

--- index.js
+++ index.js
@@ -1,81 +1,42 @@
-const RedditScraper = require("reddit-scraper");
 const express = require("express");
 const app = express();
-const NodeCache = require("node-cache");
-const cache = new NodeCache({
-  stdTTL: 7200
-});
+const LRU = require("lru-cache");
+const cache = new LRU({ max: 500, maxAge: 1000 * 3600 });
+const axios = require('axios');
+app.use(express.json());
+
+/*
 app.use("/url/:sub", (req, res, next) => {
   res.set('Cache-Control', 'public,max-age=21600');
   next();
 })
-app.use(express.json());
-const redditScraperOptions = {
-  AppId: "atU4rU0qYTpyjA",
-  AppSecret: "G6mDHqkYPR4CUiwSjyJJzVJXv6s"
-};
-const redditScraper = new RedditScraper.RedditScraper(redditScraperOptions);
-console.log("Configuration Loaded!");
+*/
 const scrapedata = async (SubReddit) => {
   try {
-    class Memeobj {
-      constructor(Sub) {
-        this.Pages = 4;
-        this.Records = 25;
-        this.SortType = "top";
-        this.SubReddit = Sub;
-        /* this.Info = null; */
-        /* this.urls = []; */
-      }
-    };
-    const obj = new Memeobj(SubReddit);
-    const memeData = await redditScraper.scrapeData(obj);
-    const memeUrls = await memeData.map(obj => obj.data.url);
-    const imgUrl = memeUrls.filter(name => name.includes('.jpg') || name.includes('.png') || name.includes('.jpeg') || name.includes('.gif'));
-    return imgUrl;
-  } catch (err) {
-    console.log('error in scrape');
-  }
+    //const imgUrl = await axios.get(`https://reddit-zeit.now.sh/url/${SubReddit}`)
+    const imgUrl = await axios.get(`https://reddit-zeit-git-url.meme69.now.sh/url/${SubReddit}`).then(resp => { return (resp.data); });
+    const imgTitle =await axios.get(`https://reddit-zeit-git-title.meme69.now.sh/url/${SubReddit}`).then(resp => { return (resp.data); });
+    return { imgUrl, imgTitle };
+  } catch (err) {console.log(err);}
 };
+
 const check = (req, res, next) => {
-  const {
-    sub
-  } = req.params;
+  const {sub} = req.params;
   if (cache.has(sub)) {
     res.json(JSON.parse(cache.get(sub)));
-    console.log(`${sub} has been cached`);
+    console.log(`${sub} already in cache`);
   } else {
     next();
   };
 };
-/*
-const set = async (req, res, next) => {
-  const sub = req.params.sub;
-  if (cache.has(sub)) {
-    const content = JSON.parse(cache.get(sub));
-    return res.status(200).send(content);
-  };
-  try {
-    const Urls = await scrapedata(sub);
-    const urls = JSON.stringify(Urls);
-    console.log(Urls);
-    cache.set(sub, urls);
-    return res.send(Urls);
-    next();
-  } catch (err) {
-    console.log(err);
-    res.status(500);
-  }
-};
-*/
+
 app.get("/url/:sub", check, async (req, res) => {
   try {
-    const {
-      sub
-    } = req.params;
+    const {sub} = req.params;
     const content = await scrapedata(sub);
     const strContent = JSON.stringify(content);
     cache.set(sub, strContent);
+    console.log(`caching ${sub}`);
     return res.json(content);
   } catch (err) {
     console.log('error in get');
@@ -85,6 +46,7 @@ app.get("/url/:sub", check, async (req, res) => {
 app.get("/", (req, res) => {
   res.send('type subredit after /');
 });
+
 app.get("/uptime", (req, res) => {
   function format(seconds) {
     function pad(s) {
@@ -98,12 +60,8 @@ app.get("/uptime", (req, res) => {
   var uptime = process.uptime();
   res.send(`${format(uptime)}`);
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`server has started in ${port}`);
 });
-
-const RedditScraper = require("reddit-scraper"); const express = require("express"); const app = express();
-ENGLISH
-const RedditScraper = require("reddit-scraper"); const express = require("express"); const app = express();
-EXTENSION OPTIONSMORE »
